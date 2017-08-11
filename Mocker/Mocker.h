310b777643fc8aa286c6d16a1a7d139e3683d183
Antoine van der Lee <antoine@wetransfer.com>
2017-08-11 11:27:32 +0200

    Created a framework

---
+++ Mocker/Mocker.h
@@ -0,0 +1,19 @@
+//
+//  Mocker.h
+//  Mocker
+//
+//  Created by Antoine van der Lee on 11/08/2017.
+//  Copyright © 2017 WeTransfer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mocker.
+FOUNDATION_EXPORT double MockerVersionNumber;
+
+//! Project version string for Mocker.
+FOUNDATION_EXPORT const unsigned char MockerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mocker/PublicHeader.h>
+
+
