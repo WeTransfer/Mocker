7e5c2160a00fcca616cd6895832e1019b0fda585
Joe Heck <heckj@mac.com>
2019-07-06 16:45:09 -0700

    fixing swiftlint warning/complaint

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "Mocker",
-            targets: ["Mocker"]),
+            targets: ["Mocker"])
     ],
     targets: [
         .target(
