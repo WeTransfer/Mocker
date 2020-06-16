855cdb3ec19883e6f5e4bcdd5c33557794287411
Tyler Morrison <haikusw@gmail.com>
2020-06-16 10:53:37 -0700

    Add my understanding of the two Tests AvdLee suggested
    Might be mis-understanding so it seemed best to write my understanding in code to aid communication.

--- MockerTests/DynamicMocksTests.swift
+++ MockerTests/DynamicMocksTests.swift
@@ -66,6 +66,130 @@ class DynamicMocksTests: XCTestCase {
 
         wait(for: [expectFailure], timeout: 2.0)
     }
+
+    func FAILS_testOnMockForConditionalReplacementAvdLeeSuggestion1() {
+        let service = FakeService() // the service we are testing
+
+        // Create Mock data
+        let goodMockData = "{\"name\":\"Joe Jones\"}".data(using: .utf8)!
+        let goodTokenData = "{\"token\":\"good token\"}".data(using: .utf8)!
+
+        // Create our three Mocks:
+
+        let succeedingProfileMock = Mock(url: FakeService.FakeServiceURLs.profileURL,
+                                         dataType: .json,
+                                         statusCode: 200,
+                                         data: [.get: goodMockData])
+        // succeedingMock.register() - Do NOT call now as will get replaced by failingMock.
+        // Instead, try registering it in the `onRequest` handler for failingProfileMock
+        // if the token has been updated
+
+        var failingProfileMock = Mock(url: FakeService.FakeServiceURLs.profileURL,
+                                      dataType: .json,
+                                      statusCode: 401,
+                                      data: [.get: Data()])
+
+        failingProfileMock.onRequest = { _, _ in
+            // this won't help because it's too late.
+            if FakeService.accessToken.token == "good token" {
+                succeedingProfileMock.register()
+            }
+        }
+        failingProfileMock.register()
+
+        let refreshTokenMock = Mock(url: FakeService.FakeServiceURLs.refreshTokenURL,
+                                    dataType: .json,
+                                    statusCode: 200,
+                                    data: [.get: goodTokenData])
+        refreshTokenMock.register()
+
+        // Now set up our dynamic mock that will replace the failingProfileMock
+        // with the succeedingProfileMock IFF we have a valid token (if the refreshToken call is made
+        // and gets a valid token)
+
+        //        Mocker.onMockFor = { _ in
+        //            if FakeService.accessToken.token == "good token" {
+        //                return succeedingProfileMock
+        //            } else { return nil }
+        //        }
+
+        let expectFailure = expectation(description: "call url - should get error")
+
+        // Now make the profile call that we expect will fail, internally
+        // get a new token from the service, and then retry and finally return us valid profile data.
+        service.profile(for: "JJ") { profile, error in
+            XCTAssertNil(error, "error is not nil!")
+            XCTAssertNotNil(profile, "profile is nil")
+            XCTAssertEqual(profile?.name, "Joe Jones", "profile name doesn't match")
+            expectFailure.fulfill()
+        }
+
+        wait(for: [expectFailure], timeout: 2.0)
+    }
+
+    func FAILS_testOnMockForConditionalReplacementAvdLeeSuggestion2() {
+        let service = FakeService() // the service we are testing
+
+        // Create Mock data
+        let goodMockData = "{\"name\":\"Joe Jones\"}".data(using: .utf8)!
+        let goodTokenData = "{\"token\":\"good token\"}".data(using: .utf8)!
+
+        // Create our three Mocks:
+
+        let succeedingProfileMock = Mock(url: FakeService.FakeServiceURLs.profileURL,
+                                         dataType: .json,
+                                         statusCode: 200,
+                                         data: [.get: goodMockData])
+        // succeedingMock.register() - Do NOT call now as will get replaced by failingMock.
+        // Instead, try registering it in the `onRequest` handler for failingProfileMock
+        // if the token has been updated
+
+        let failingProfileMock = Mock(url: FakeService.FakeServiceURLs.profileURL,
+                                      dataType: .json,
+                                      statusCode: 401,
+                                      data: [.get: Data()])
+        failingProfileMock.register()
+
+        var refreshTokenMock = Mock(url: FakeService.FakeServiceURLs.refreshTokenURL,
+                                    dataType: .json,
+                                    statusCode: 200,
+                                    data: [.get: goodTokenData])
+
+        refreshTokenMock.onRequest = { _, _ in
+            // This won't help because it's too early.
+            // Removing the if-test means we are *assuming* that the refresh payload
+            // processing code worked, but that's part of the code under test so
+            // we do not want to do that.
+            if FakeService.accessToken.token == "good token" {
+                succeedingProfileMock.register()
+            }
+        }
+
+        refreshTokenMock.register()
+
+        // Now set up our dynamic mock that will replace the failingProfileMock
+        // with the succeedingProfileMock IFF we have a valid token (if the refreshToken call is made
+        // and gets a valid token)
+
+        //        Mocker.onMockFor = { _ in
+        //            if FakeService.accessToken.token == "good token" {
+        //                return succeedingProfileMock
+        //            } else { return nil }
+        //        }
+
+        let expectFailure = expectation(description: "call url - should get error")
+
+        // Now make the profile call that we expect will fail, internally
+        // get a new token from the service, and then retry and finally return us valid profile data.
+        service.profile(for: "JJ") { profile, error in
+            XCTAssertNil(error, "error is not nil!")
+            XCTAssertNotNil(profile, "profile is nil")
+            XCTAssertEqual(profile?.name, "Joe Jones", "profile name doesn't match")
+            expectFailure.fulfill()
+        }
+
+        wait(for: [expectFailure], timeout: 2.0)
+    }
 }
 
 // MARK: - Example Service Under Test -
@@ -168,4 +292,3 @@ class FakeService {
         }.resume()
     }
 }
-
