bde694018c2fad3a35400384ce68c3b64adb414d
Alexander Weiß <weiss_alex@gmx.net>
2022-07-26 14:06:24 +0200

    fix: Apply PR suggestions

--- Sources/Mock+DataType.swift
+++ Sources/Mock+DataType.swift
@@ -12,11 +12,10 @@ extension Mock {
     /// The types of content of a request. Will be used as Content-Type header inside a `Mock`.
     public struct DataType {
 
-        /// Name of the data type
+        /// Name of the data type.
         public let name: String
 
-
-        /// The header value of the data type
+        /// The header value of the data type.
         public let headerValue: String
 
         public init(name: String, headerValue: String) {
@@ -27,10 +26,10 @@ extension Mock {
 }
 
 extension Mock.DataType {
-    public static var json = Mock.DataType(name: "json", headerValue: "application/json; charset=utf-8")
-    public static var html = Mock.DataType(name: "html", headerValue: "text/html; charset=utf-8")
-    public static var imagePNG = Mock.DataType(name: "imagePNG",headerValue: "image/png")
-    public static var pdf = Mock.DataType(name: "pdf",headerValue: "application/pdf")
-    public static var mp4 = Mock.DataType(name: "mp4",headerValue: "video/mp4")
-    public static var zip = Mock.DataType(name: "zip",headerValue: "application/zip")
+    public static let json = Mock.DataType(name: "json", headerValue: "application/json; charset=utf-8")
+    public static let html = Mock.DataType(name: "html", headerValue: "text/html; charset=utf-8")
+    public static let imagePNG = Mock.DataType(name: "imagePNG", headerValue: "image/png")
+    public static let pdf = Mock.DataType(name: "pdf", headerValue: "application/pdf")
+    public static let mp4 = Mock.DataType(name: "mp4", headerValue: "video/mp4")
+    public static let zip = Mock.DataType(name: "zip", headerValue: "application/zip")
 }
