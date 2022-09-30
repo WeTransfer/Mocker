c57953ce42de4ff4f17ae0ca28ecc19696577370
Antoine van der Lee <ajvanderlee@gmail.com>
2022-09-30 10:48:10 +0200

    Add workflow for automatically building docs

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 // We're hiding dev, test, and danger dependencies with // dev to make sure they're not fetched by users of this package.
 
@@ -17,6 +17,7 @@ let package = Package(name: "Mocker",
                       dependencies: [
                         // dev .package(name: "danger-swift", url: "https://github.com/danger/swift", from: "3.12.1"),
                         // dev .package(name: "WeTransferPRLinter", path: "Submodules/WeTransfer-iOS-CI/WeTransferPRLinter")
+                        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0")
                         ],
                       targets: [
                         // dev .target(name: "DangerDependencies", dependencies: [
